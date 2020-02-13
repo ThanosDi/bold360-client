974305c0d331a819a5260f3a2d040669c6a66423
PowerPC <thanosdi@live.com>
2020-02-13 10:10:08 +0200

    Refactor

--- src/index.test.js
+++ src/index.test.js
@@ -1,27 +1,42 @@
 const fetch = require('jest-fetch-mock');
-const { bold360Client } = require('.');
+const {bold360Client} = require('.');
 
 const credentials = {
-	accountId: process.env.BOLD360_ACCOUNT_ID || 11111111, 
-	apiSettingId: process.env.BOLD360_API_SETTINGS_ID || 2222222, 
-	apiKey: process.env.BOLD360_API_KEY ||'yqsTq6i9A9v+YLYdi7CEbAfH9ai8TKFx',
-}
+	accountId: 11111111,
+	apiSettingId: 2222222,
+	apiKey: 'yqsTq6i9A9v+YLYdi7CEbAfH9ai8TKFx',
+};
 
 describe('bold360Client', () => {
-
 	beforeEach(() => jest.resetAllMocks());
 
 	test('fetch getChat', async () => {
-		await bold360Client(credentials, 'getChat', {ChatID:'107984128488260564'}, {}, fetch);
+		await bold360Client(
+			credentials,
+			'getChat',
+			{ChatID: '107984128488260564'},
+			{},
+			fetch,
+		);
 		expect(fetch).toHaveBeenCalled();
 		expect(fetch).toHaveBeenCalledTimes(1);
-		expect(fetch).toHaveBeenCalledWith(expect.toStartWith('https://api-eu.boldchat.com/aid/106562681705387893/data/rest/json/v1/getChat?ChatID=107984128488260564&auth='), {});
+		expect(fetch).toHaveBeenCalledWith(
+			expect.toStartWith(
+				'https://api-eu.boldchat.com/aid/11111111/data/rest/json/v1/getChat?ChatID=107984128488260564&auth=11111111',
+			),
+			{},
+		);
 	});
 
 	test('fetch allChats', async () => {
 		await bold360Client(credentials, 'allChats', {}, {}, fetch);
 		expect(fetch).toHaveBeenCalled();
 		expect(fetch).toHaveBeenCalledTimes(1);
-		expect(fetch).toHaveBeenCalledWith(expect.toStartWith('https://api-eu.boldchat.com/aid/106562681705387893/data/rest/json/v1/allChats?auth='), {});
+		expect(fetch).toHaveBeenCalledWith(
+			expect.toStartWith(
+				'https://api-eu.boldchat.com/aid/11111111/data/rest/json/v1/allChats?auth=11111111',
+			),
+			{},
+		);
 	});
 });
--- src/make-bold360-url.js
+++ src/make-bold360-url.js
@@ -1,27 +1,23 @@
-const {
-	equals,
-	ifElse,
-} = require('ramda');
+const {equals, ifElse} = require('ramda');
 const queryString = require('query-string');
 const {sha512} = require('js-sha512');
 
 const europeUrl = 'https://api-eu.boldchat.com';
-const usaUrl = 'https://api.boldchat.com';
+const usUrl = 'https://api.boldchat.com';
 
-const pickServer =
-	ifElse(
-		equals('eu'),
-		() => europeUrl,
-		() => usaUrl,
-	);
+const pickServer = ifElse(
+	equals('eu'),
+	() => europeUrl,
+	() => usUrl,
+);
 
-const createAuth = ({accountId,apiSettingId, apiKey}) => {
+const createAuth = ({accountId, apiSettingId, apiKey}) => {
 	const time = Date.now();
 	const auth = `${accountId}:${apiSettingId}:${time}`;
 	const hashAuth = sha512(`${auth}${apiKey}`);
 
 	return `${auth}:${hashAuth}`;
-}
+};
 
 /**
  * Returns the api url for Bold360 to use. Given the credentials or the region the url will vary.
@@ -30,10 +26,16 @@ const createAuth = ({accountId,apiSettingId, apiKey}) => {
  * @param {String} params
  * @returns {String}
  */
-const makeUrl = ({accountId, apiSettingId, apiKey, region = 'eu'}, path, params) => {
+const makeUrl = (
+	{accountId, apiSettingId, apiKey, region = 'eu'},
+	path,
+	params,
+) => {
 	const auth = createAuth({accountId, apiSettingId, apiKey});
-	const url = `${pickServer(region)}/aid/${accountId}/data/rest/json/v1/${path}`;
+	const url = `${pickServer(
+		region,
+	)}/aid/${accountId}/data/rest/json/v1/${path}`;
 	return queryString.stringifyUrl({url, query: {...params, auth}});
-}
+};
 
 module.exports = {makeUrl};
